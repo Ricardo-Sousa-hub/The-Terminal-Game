befd9aa8fe437bf8190b0d91639dc5b3ed990aaa
Ricardo Sousa <58951336+Ricardo-Sousa-hub@users.noreply.github.com>
2021-03-22 12:58:39 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+#  Stats de cada arma
+
+faca = {"Dano": 50,
+        "D/s": 50,
+        "Espaço no inventario":3
+        }
+
+espada = {"Dano": 100,
+          "D/s": 100,
+          "Espaço no inventario": 7
+          }
+
+glock = {"Dano": 15,
+         "D/s": 30,
+         "Espaço no invetario": 4}
+
+#  Armas
+
+armas = [faca, espada, glock]
+
+#  Nivel de jogador
+
+playerlvl = 0
+
+#  Niveis de inimigos
+
+lvl1 = ["esquivar", "esquivar", "esquivar", "atacar"]
+
+lvl2 = ["esquivar", "esquivar", "atacar", "atacar"]
+
+lvl3 = ["esquivar", "atacar", "atacar", "atacar"]
+
+print("The LastTry in terminal")
+
+nome_de_utilizador = str(input("Digite o nome de utilizador? "))
+
+
